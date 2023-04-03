9acf591954051e85d310aba8ccb707d8130c08c4
Andy Chea <achea3@gatech.edu>
2023-04-02 23:08:37 -0400

    ADD: Began CSS file, generated HTML base and changed index.html to extend it.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 #Might need to use virtualenv if conda doesn't cooperate
 
 #Generate flask template for later builds 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 @app.route('/')
