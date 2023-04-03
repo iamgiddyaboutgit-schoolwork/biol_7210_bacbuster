e8e9b4854e04a3c4edbbb1c6460818737988c4d0
Andy Chea <achea3@gatech.edu>
2023-04-02 22:47:29 -0400

    Added html index file for designing webpage.

--- app.py
+++ app.py
@@ -3,13 +3,13 @@
 #Might need to use virtualenv if conda doesn't cooperate
 
 #Generate flask template for later builds 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return "Hello, World!"
+    return redner_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
