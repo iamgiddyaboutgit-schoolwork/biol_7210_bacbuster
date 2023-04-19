36618c1d409fb025c6692b4150285ab9f0f28c13
Andy C <andychea2000@gmail.com>
2023-04-18 22:30:46 -0400

    Fleshed out comments, changed helptext

--- stApp.py
+++ stApp.py
@@ -4,13 +4,14 @@ import streamlit as st
 st.title(":boom: BacBuster Isolate Analyzer :boom:")
 
 #Provide some info on how the app works and the authors.
-st.text("This app seeks to simplify the process of analyzing raw paired reads from outbreak isolates. Users can ask for a readout of antimicrobial genes identified, or stop the pipeline at a preliminary step.")
+st.text("This app simplifies analysis of raw paired reads from outbreak isolates.\nUsers can process the samples up to the AMR annotation, but can stop before.")
+
 
 #Create form so that uploads and email are all processed together.
 with st.form("func"):
 
     #Give the user the ability to upload isolates TODO Increase file size limit and theming.
-    upload = st.file_uploader("Submit raw paired reads of the isolate(s).", accept_multiple_files=True)
+    upload = st.file_uploader("Upload raw paired reads of the isolate(s).", accept_multiple_files=True)
 
 
     #Add a radio button that allows the user to decide on how much of the pipeline should be run.
@@ -18,16 +19,18 @@ with st.form("func"):
     st.write("⚠️ NOTE: Selecting a later step will increase the runtime and size of the final results folder.")
 
     #Add a text input for email. TODO make this optional.
-    email = st.text_input("Provide email for to receive results later.")
+    email = st.text_input("Provide email if you would like to receive results later.", placeholder="kingjordan@gmail.com")
 
-    #Submit button for the form.
+    #Submit button for the form. TODO disable on click? A mistaken submission would need to reopen the app then.
     submitted = st.form_submit_button()
     if submitted:
         st.write("Inputs provided. Please wait for results!\n Note that pressing the submit button again will reset the pipeline.")
 
-#RUN THE NEXTFLOW PIPELINE AND RETURN RESULTS
+#RUN THE NEXTFLOW PIPELINE AND RETURN RESULTS USING SUBPROCESS.
 import subprocess
 
+
+#THESE WIDGETS SHOULD ONLY BE DRAWN AFTER SUBMIT IS PRESSED.
 #Find out how many pairs of files there are.
 pairCount = len(upload) / 2 # if no files, returns 0.
 #Check that this is at least 1 (the user has input at least 2 files.)
@@ -36,27 +39,30 @@ if pairCount == 0:
 
 
 #Assembly Step
+#subprocess(nextflow run )
 
 
 #Notify user that assembly is finished.
-st.success('Assembly completed!', icon="✅")
+st.success(' Assembly completed!', icon="✅")
 
 #Prediction Step
 
 
 #Notify user that prediction is finished.
 
-st.success('Prediction completed! If Functional Annotation was selected, hold tight! Results incoming.', icon="✅")
+st.success(' Prediction completed! If Functional Annotation was selected, hold tight! Results incoming.', icon="✅")
 #Prediction step completed 2/3, hold tight!
 
 #Annotation Step
 
 #Write to results screen. Before completion, have some sort of progress printed to user.
+#st.write() handles many data inputs, such as images.
 
-#Provide download button for the results.
-#db = st.download_button()
+
+#Provide download button for the results. TODO Hide this until the pipeline has finished running later.
+#db = st.download_button("Download zipped final results.", f, file_name="results.zip")
 
 #Email results to input email using smtp. Wait until the app has been tested without.
 if email is not None:
     #Send email
-    print("email provided")
+    st.write("email provided")
