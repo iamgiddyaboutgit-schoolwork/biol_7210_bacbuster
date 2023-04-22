7f4fda76e77a3f032192f9968b2b48dc9d4c7fa4
jpatterson87 <jpatterson87@gatech.edu>
2023-04-22 12:32:22 -0400

    Add info on contexts
    There are 2 contexts in Flask:
    1. application context
    2. request context

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+# This is a view function that tells the Flask app instance
+# what code to run.  Navigation on the website to the route
+# causes this view function to execute.  This view function
+# returns a response to the client. 
+# Through the magic of contexts, view functions like the one
+# below can get access to global objects.  Contexts are nice
+# because they don't cause clashes across threads and multiple
+# users.
+@app.route("/")
+def index():
+    user_agent = request.headers.get("User-Agent")
+    return f"<h1>BacBuster will be used on your browser of {user_agent}.</h1>"
+
+
+#####################################################################
+if __name__ == '__main__': 
+    # Do not use run() in a production setting. It is not intended 
+    # to meet security and performance requirements for a production 
+    # server.
+    app.run(debug=True)
